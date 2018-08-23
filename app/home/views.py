794170ca32fe58875a4a4cb1db79d6e0571f8639
vannesspeng1990 <401041628@qq.com>
2018-08-23 11:34:47 +0800

    首页路由，登录，注册前端网页完成

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,5 @@
 from . import home
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 
 @home.route('/')
@@ -7,11 +7,18 @@ def index():
     return render_template("home/index.html")
 
 
+# 登录
 @home.route('/login/')
 def login():
     return render_template('home/login.html')
 
-
+# 退出
 @home.route('/logout/')
 def logout():
-    return render_template('home/login.html')
+    return redirect(url_for('home.login'))
+#重定向到home模块下的登录
+
+# 注册
+@home.route('/register/')
+def register():
+    return render_template('home/register.html')
