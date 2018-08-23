0e5fb64a43d66218d4c3aa9994a51fb8d769f3b6
vannesspeng1990 <401041628@qq.com>
2018-08-23 17:14:37 +0800

    前台404页面 后台登录、首页、密码修改页面

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -9,3 +9,10 @@ from app.home import home as home_blueprint
 #1、注册蓝图
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
 app.register_blueprint(home_blueprint)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    404
+    """
+    return render_template("home/404.html"), 404
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,32 @@
+from flask import render_template, redirect, url_for
+
 from . import admin
 
 @admin.route('/')
 def index():
-    return "<h1 style='color:green'>this is admin</h1>"
+    return render_template("admin/index.html")
+
+
+@admin.route("/login/")
+def login():
+    """
+    后台登录
+    """
+    return render_template("admin/login.html")
+
+
+@admin.route("/logout/")
+def logout():
+    """
+    后台注销登录
+    """
+    return redirect(url_for("admin.login"))
+
+@admin.route("/pwd/")
+def pwd():
+    """
+    后台登录
+    """
+    return render_template("admin/pwd.html")
+
+
